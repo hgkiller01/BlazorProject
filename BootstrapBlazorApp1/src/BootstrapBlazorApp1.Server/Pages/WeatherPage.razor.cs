b33cb576c3715a35347b4494a0e96bf68e5e2502
克雷斯 <hgkiller@hotmail.com>
2020-11-19 13:48:24 +0800

    修改

--- BootstrapBlazorApp1/src/BootstrapBlazorApp1.Server/Pages/WeatherPage.razor.cs
+++ BootstrapBlazorApp1/src/BootstrapBlazorApp1.Server/Pages/WeatherPage.razor.cs
@@ -14,8 +14,16 @@ namespace BootstrapBlazorApp1.Server.Pages
     public partial class WeatherPage
     {
         public List<Location> AllData { get; set; }
-        public List<SelectedItem> Items { get; set; }
+        public List<SelectedItem> Items { get; set; } = new List<SelectedItem>()
+            {
+                new SelectedItem(){ Text = "天氣狀態" , Value ="Wx",Active = true},
+                new SelectedItem(){Text = "下雨機率" , Value = "PoP" },
+                new SelectedItem(){Text = "最低氣溫" , Value = "MinT"},
+                new SelectedItem(){Text = "最高氣溫" , Value = "MaxT"},
+                new SelectedItem(){Text = "體感",Value = "CI"}
+            };
         public string SWeatherType { get; set; } = "Wx";
+        public string Status { get; set; } = "天氣狀態";
         [Inject]
         public IConfiguration Config { get; set; }
         [Inject]
@@ -24,19 +32,17 @@ namespace BootstrapBlazorApp1.Server.Pages
         {
             Weathers Souce = await Http.GetFromJsonAsync<Weathers>(Config["ApiUrl"] + "/api/Weather");
             AllData = Souce.records.location;
-            Items = new List<SelectedItem>()
-            {
-                new SelectedItem(){ Text = "天氣狀態" , Value ="Wx",Active = true},
-                new SelectedItem(){Text = "下雨機率" , Value = "PoP" },
-                new SelectedItem(){Text = "最低氣溫" , Value = "MinT"},
-                new SelectedItem(){Text = "最高氣溫" , Value = "MaxT"},
-                new SelectedItem(){Text = "體感",Value = "CI"}
-            };
         }
         protected Weatherelement GetWeatherElement(List<Weatherelement> weathers , string WeatherType)
         {
             return weathers.Where(x => x.elementName == WeatherType).FirstOrDefault();
         }
-
+        public async Task<Task> ChangeDropDown(SelectedItem selectedItem)
+        {
+            Status = selectedItem.Text;
+            Weathers Souce = await Http.GetFromJsonAsync<Weathers>(Config["ApiUrl"] + "/api/Weather");
+            AllData = Souce.records.location;
+            return Task.CompletedTask;
+        }
     }
 }
--- BootstrapBlazorApp1/src/BootstrapBlazorApp1.Server/Shared/MainLayout.razor.cs
+++ BootstrapBlazorApp1/src/BootstrapBlazorApp1.Server/Shared/MainLayout.razor.cs
@@ -25,37 +25,14 @@ namespace BootstrapBlazorApp1.Server.Shared
         {
             var ret = new List<MenuItem>
             {
-                new MenuItem() { Text = "返回组件库", Icon = "fa fa-fw fa-home", Url = "https://blazor.sdgxgz.com/components" },
-                new MenuItem() { Text = "Index", Icon = "fa fa-fw fa-fa", Url = "" },
-                new MenuItem() { Text = "Counter", Icon = "fa fa-fw fa-check-square-o", Url = "counter" },
-                new MenuItem() { Text = "FetchData", Icon = "fa fa-fw fa-database", Url = "fetchdata" },
-                new MenuItem() { Text = "系统设置", Icon = "fa fa-fw fa-gears", Url = "#" },
-                new MenuItem() { Text = "权限设置", Icon = "fa fa-fw fa-users", Url = "#" },
-                new MenuItem() { Text = "日志设置", Icon = "fa fa-fw fa-database", Url = "#" }
+                //new MenuItem() { Text = "返回组件库", Icon = "fa fa-fw fa-home", Url = "https://blazor.sdgxgz.com/components" },
+                new MenuItem() { Text = "首頁", Icon = "fa fa-fw fa-fa", Url = "" },
+                new MenuItem() { Text = "天氣預報", Icon = "fa fa-fw fa-check-square-o", Url = "weatherPage" },
+                //new MenuItem() { Text = "FetchData", Icon = "fa fa-fw fa-database", Url = "fetchdata" },
+                //new MenuItem() { Text = "系统设置", Icon = "fa fa-fw fa-gears", Url = "#" },
+                //new MenuItem() { Text = "权限设置", Icon = "fa fa-fw fa-users", Url = "#" },
+                //new MenuItem() { Text = "日志设置", Icon = "fa fa-fw fa-database", Url = "#" }
             };
-
-            ret[4].AddItem(new MenuItem() { Text = "网站设置", Icon = "fa fa-fw fa-fa" });
-            ret[4].AddItem(new MenuItem() { Text = "任务设置", Icon = "fa fa-fw fa-tasks" });
-            ret[4].AddItem(new MenuItem() { Text = "用户设置", Icon = "fa fa-fw fa-user" });
-            ret[4].AddItem(new MenuItem() { Text = "菜单设置", Icon = "fa fa-fw fa-dashboard" });
-            ret[4].AddItem(new MenuItem() { Text = "角色设置", Icon = "fa fa-fw fa-sitemap" });
-            ret[4].AddItem(new MenuItem() { Text = "访问日志", Icon = "fa fa-fw fa-bars" });
-            ret[4].AddItem(new MenuItem() { Text = "登录日志", Icon = "fa fa-fw fa-user-circle-o" });
-            ret[4].AddItem(new MenuItem() { Text = "操作日志", Icon = "fa fa-fw fa-edit" });
-
-            ret[5].AddItem(new MenuItem() { Text = "用户设置", Icon = "fa fa-fw fa-user" });
-            ret[5].AddItem(new MenuItem() { Text = "菜单设置", Icon = "fa fa-fw fa-dashboard" });
-            ret[5].AddItem(new MenuItem() { Text = "角色设置", Icon = "fa fa-fw fa-sitemap" });
-            ret[5].AddItem(new MenuItem() { Text = "访问日志", Icon = "fa fa-fw fa-bars" });
-            ret[5].AddItem(new MenuItem() { Text = "登录日志", Icon = "fa fa-fw fa-user-circle-o" });
-            ret[5].AddItem(new MenuItem() { Text = "操作日志", Icon = "fa fa-fw fa-edit" });
-
-            ret[6].AddItem(new MenuItem() { Text = "用户设置", Icon = "fa fa-fw fa-user" });
-            ret[6].AddItem(new MenuItem() { Text = "菜单设置", Icon = "fa fa-fw fa-dashboard" });
-            ret[6].AddItem(new MenuItem() { Text = "角色设置", Icon = "fa fa-fw fa-sitemap" });
-            ret[6].AddItem(new MenuItem() { Text = "访问日志", Icon = "fa fa-fw fa-bars" });
-            ret[6].AddItem(new MenuItem() { Text = "登录日志", Icon = "fa fa-fw fa-user-circle-o" });
-            ret[6].AddItem(new MenuItem() { Text = "操作日志", Icon = "fa fa-fw fa-edit" });
 
             return ret;
         }
